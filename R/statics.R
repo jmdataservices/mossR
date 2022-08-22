cc504409a9e539babcc9bbbe1e9075c8bb874f72
Jamie McLaughlin <37658247+jmoss84@users.noreply.github.com>
2022-08-22 21:44:53 +0100

    Added minor changes to theme.

--- R/statics.R
+++ R/statics.R
@@ -6,14 +6,17 @@ theme_moss <- function() {
         ggplot2::theme_minimal() +
         ggplot2::theme(
                 panel.grid = ggplot2::element_blank(),
+                panel.background = ggplot2::element_rect(fill = "white", color = "white"),
+                plot.background = ggplot2::element_rect(fill = "white", color = "white"),
                 plot.title = ggplot2::element_text(size = 22, hjust = 0.5),
                 plot.subtitle = ggplot2::element_text(size = 18, hjust = 0.5),
                 plot.caption = ggplot2::element_text(size = 11, face = "italic"),
                 axis.title = ggplot2::element_text(size = 16, face = "bold", color = "grey40"),
                 axis.text = ggplot2::element_text(size = 14, color = "grey60"),
                 strip.text = ggplot2::element_text(size = 16, face = "bold", color = "grey40"),
-                strip.background = ggplot2::element_rect(color = NULL),
+                strip.background = ggplot2::element_rect(color = "white"),
                 legend.title = ggplot2::element_blank(),
+                legend.title = element_blank(),
                 legend.position = "none"
         )
 }
