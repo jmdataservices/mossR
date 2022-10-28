86b487f4001f23af853b54fd6c8a08e43b9dc43d
Jamie McLaughlin <37658247+jmoss84@users.noreply.github.com>
2022-10-28 12:33:59 +0100

    Added multiple functions for JMDS purposes.

--- R/connections.R
+++ R/connections.R
@@ -15,8 +15,11 @@ get_data_db_azurecreds <- function(server, database, uid, pwd = "pwd_not_set", a
         database <- paste0("database=", database, ";")
         uid <- paste0("uid=", uid, ";")
         pwd <- paste0("pwd=", pwd, ";")
+
         if (auth == "ad") {
+
                 auth <- "authentication=ActiveDirectoryInteractive;"
+
                 con_str <- paste0(
                         driver,
                         server,
@@ -24,12 +27,15 @@ get_data_db_azurecreds <- function(server, database, uid, pwd = "pwd_not_set", a
                         uid,
                         auth
                 )
+
                 con <- DBI::dbConnect(
                         odbc::odbc(),
                         .connection_string = con_str,
                         timeout = 5
                 )
+
         } else if (auth == "sql") {
+
                 con_str <- paste0(
                         driver,
                         server,
@@ -37,20 +43,28 @@ get_data_db_azurecreds <- function(server, database, uid, pwd = "pwd_not_set", a
                         uid,
                         pwd
                 )
+
                 con <- DBI::dbConnect(
                         odbc::odbc(),
                         .connection_string = con_str,
                         timeout = 5
                 )
+
         } else {
+
                 print("Auth does not have an appropriate value (either 'sql' or 'ad' should be given)")
+
         }
+
         raw <- DBI::dbGetQuery(
                 conn = con,
                 statement = query
         )
+
         dbDisconnect(con)
+
         rm(con, con_str, pwd, uid, auth, database, server, driver)
+
         return(raw)
 
 }
@@ -81,4 +95,89 @@ get_data_dl_azurekey <- function(endpoint, key, container, directory) {
         )
 
         return(st_files)
+
+}
+
+
+#' A Function to Rapidly Push a File to an Azure Gen2 Data Lake
+#'
+#' @param vault_name The name of the vault (not including the full URL), just the name
+#' @param secret The name of the secret behind which the storage account key is held
+#' @param endpoint The endpoint of the data lake being accessed
+#' @param container The container into which we'll push a file
+#' @param file The name of the local file that will be published
+#' @param destination_file The full file path of the file as it will appear in storage
+
+push_blob_dl_azurekey <- function(vault_name, secret, endpoint, container, file, destination_file) {
+
+        vault <- paste0("https://", vault_name, ".vault.azure.net")
+
+        vlt <- AzureKeyVault::key_vault(
+                url = vault
+        )
+
+        seckey <- vlt$secrets$get(secret)
+        seckey <- seckey$value
+
+        st_ep <- AzureStor::adls_endpoint(
+                endpoint = endpoint,
+                key = seckey
+        )
+
+        st_cont <- AzureStor::adls_filesystem(
+                st_ep,
+                container
+        )
+
+        AzureStor::storage_upload(
+                container = st_cont,
+                src = file,
+                dest = destination_file
+        )
+
+}
+
+
+#' A Function to Publish a Record to an Azure SQL Database
+#'
+#' @param vault_name The name of the vault (not including the full URL), just the name
+#' @param uid_secret The name of the secret behind which the database user ID is held
+#' @param pwd_secret The name of the secret behind which the database password is held
+#' @param driver This parameter defaults to 'ODBC Driver 17 for SQL Server' but can be changed
+#' @param server The Azure SQL Server into which we're depositing data
+#' @param database The Azure SQL Database into which we're depositing data
+#' @param table_name The name of the table into which we're depositing data
+#' @param data_frame The data packet being passed into the table - presented as a data frame
+
+push_data_db_azurekv <- function(vault_name, uid_secret, pwd_secret, driver = "ODBC Driver 17 for SQL Server", server, database, table_name, data_frame) {
+
+        vault <- paste0("https://", vault_name, ".vault.azure.net")
+
+        vlt <- AzureKeyVault::key_vault(
+                url = vault
+        )
+
+        secuid <- vlt$secrets$get(uid_secret)
+        secuid <- secuid$value
+        secpwd <- vlt$secrets$get(pwd_secret)
+        secpwd <- secpwd$value
+
+        con <- DBI::dbConnect(
+                odbc::odbc(),
+                driver = driver,
+                server = server,
+                database = database,
+                uid = secuid,
+                pwd = secpwd,
+                timeout = 5
+        )
+
+        DBI::dbAppendTable(
+                conn = con,
+                name = DBI::SQL(table_name),
+                value = data_frame
+        )
+
+        DBI::dbDisconnect(con)
+
 }
---
+++ R/jmds_specific.R
@@ -0,0 +1,90 @@
+
+#' A Function to Automate the Pushing of an Analysis HTML file to the JMDS estate
+#'
+#' @param file The local analysis file
+#' @param destination_file The destination file within the storage account, no filepath
+
+jmds_push_analysis_storage <- function(file, destination_file) {
+
+        vault_name <- "kvjmds"
+        secret <- "stkey"
+        endpoint <- "https://storjmds.dfs.core.windows.net/"
+        container <- "jmds"
+        file <- file
+        destination_file <- paste0("jmdsplatform/analyses/", destination_file)
+
+        push_blob_dl_azurekey(
+                vault_name,
+                secret,
+                endpoint,
+                container,
+                file,
+                destination_file
+        )
+
+}
+
+
+#' A Function to Automate the Pushing of an Analysis HTML record to the JMDS estate
+#'
+#' @param destination_file The destination file within the storage account, no filepath
+#' @param analysis_name The unique name of the analysis for record-keeping in the database
+#' @param analysis_desc The description of the analysis for the database
+
+jmds_push_analysis_record <- function(destination_file, analysis_name, analysis_desc) {
+
+        vault_name <- "kvjmds"
+        uid_secret <- "sqladminuid"
+        pwd_secret <- "sqladminpass"
+        server <- "jmds.database.windows.net"
+        database <- "jmds"
+        table_name <- "mdp.Analyses"
+        data_frame <- data.frame(
+                CompanyID = 1,
+                AnalysisName = analysis_name,
+                Description = analysis_desc,
+                SensitivityLevel = 0,
+                isTest = 0,
+                APICall = gsub(".html", "", destination_file),
+                Location = paste0("jmdsplatform/analyses/", destination_file)
+        )
+
+        push_data_db_azurekv(
+                vault_name = vault_name,
+                uid_secret = uid_secret,
+                pwd_secret = pwd_secret,
+                server = server,
+                database = database,
+                table_name = table_name,
+                data_frame = data_frame
+        )
+
+}
+
+
+#' A Function to Automate the Pushing of an Analysis to the JMDS estate
+#'
+#' @param input_filename The locally held Rmd file for analysis
+#' @param output_filename The file name of the HTML analysis output
+#' @param analysis_name The unique name of the analysis within the database
+#' @param analysis_desc The description of the analysis for the database
+
+jmds_push_analysis <- function(input_filename, output_filename, analysis_name, analysis_desc) {
+
+        rmarkdown::render(
+                input = input_filename,
+                output_file = output_filename
+        )
+
+        jmds_push_analysis_storage(
+                file = output_filename,
+                destination_file = output_filename
+        )
+
+        jmds_push_analysis_record(
+                destination_file = output_filename,
+                analysis_name = analysis_name,
+                analysis_desc = analysis_desc
+        )
+
+}
